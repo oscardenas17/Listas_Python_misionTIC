afa2408d8824a9ce33a3726739f2b1fc554cfd87
OscarYesidYesid <>
2021-05-28 00:17:26 +0000

    Initial commit

---
+++ funciones_listas.py
@@ -0,0 +1,45 @@
+""" Modulo Listas
+    Funciones para practicas con listas
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+# Definición de Funciones
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+def suma_acum(lista_num):
+  #TODO Comentar código
+  #TODO Implementar la función
+  sumatoria=0
+  listac=[]
+
+  for i in lista_num:
+    sumatoria = sumatoria+i
+    listac.append(sumatoria)
+    
+    
+
+  return    
+
+
+def suma_acumulativa(lista_num):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return 
+
+def traductor_pig_latin(lista_palabras):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return 
+
+def lista_pult(lista_num):
+  return print([lista_num[0], lista_num[len(lista_num)-1]])
+
+def identificador_frutas(lista_frutas):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def son_palindromos(frase_uno, frase_dos):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
---
+++ main.py
@@ -0,0 +1,7 @@
+import funciones_listas as flis
+
+listan=[12,34,8,10,5]
+#flis.suma_acumulativa(listan)
+#flis.lista_pult(listan)
+#listapal=["oso","carro","aro","cima"]
+#flis.traductor_pig_latin(listapal)
